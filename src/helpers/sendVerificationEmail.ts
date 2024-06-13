df1b289fbd73fed10aab7bdf717dd9c3a4397042
Manpreet Singh <manpreetmss400@gmail.com>
2024-06-13 09:28:45 +0530

    setup resend for sending verification email for signup api

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,28 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string,
+): Promise<ApiResponse> {
+    try {
+
+        const { data, error } = await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: email,
+            subject: 'Ama-app Verification Code',
+            react: VerificationEmail({ username, otp: verifyCode }),
+        });
+
+        console.log("sendVerificationEmail results data :: ", data);
+        console.log("sendVerificationEmail results error :: ", error);
+
+        return { success: true, message: "Verification message send successfully." };
+    } catch (error) {
+        console.error("error sending verification emal :: ", error);
+        return { success: false, message: "Failed to send verification email." };
+    }
+}
